eff99e9090f33c6eeda948a351d135138ae1bd28
Yepez1997 <yepez4845@gmail.com>
2019-08-13 14:42:02 -0700

    greet pb

--- src/gRPC/greet/greet_server/main.go
+++ src/gRPC/greet/greet_server/main.go
@@ -1,20 +1,31 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net"
 
+	"github.com/Yepez1997/goProjects/src/gRPC/greet/greetpb"
 	"google.golang.org/grpc"
 )
 
+// all services - added on needed basis
 type server struct{}
 
 func main() {
 
-	listener, err := net.Listen("tcp", "0.0.0.0.50051")
+	fmt.Print("Listening on port 50051 ...")
+	// create connection; and port binding
+	listener, err := net.Listen("tcp", "0.0.0.0:50051")
 	if err != nil {
-		log.Fatalf("Failed to listen %v", err)
+		log.Fatalf("Failed to listen: %v", err)
 	}
 	s := grpc.NewServer()
+	// the path to the protocol buffer
+	greetpb.RegisterGreetServiceServer(s, &server{})
+
+	if err := s.Serve(listener); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
 
 }
