3776220014c6162eae66a563df9eeecea59d77be
Yepez1997 <yepez4845@gmail.com>
2019-08-20 17:02:34 -0700

    Setting up mongo db client

--- go.mod
+++ go.mod
@@ -1,7 +1,14 @@
 module github.com/Yepez1997/goProjects
 
 require (
+	github.com/go-stack/stack v1.8.0 // indirect
 	github.com/golang/protobuf v1.3.2
+	github.com/golang/snappy v0.0.1 // indirect
 	github.com/gorilla/mux v1.7.3
+	github.com/mongodb/mongo-go-driver v1.1.0 // indirect
+	github.com/stretchr/testify v1.4.0 // indirect
+	github.com/xdg/scram v0.0.0-20180814205039-7eeb5667e42c // indirect
+	github.com/xdg/stringprep v1.0.0 // indirect
+	go.mongodb.org/mongo-driver v1.1.0 // indirect
 	google.golang.org/grpc v1.22.1
 )
--- src/gRPC/blog/blog_server/server.go
+++ src/gRPC/blog/blog_server/server.go
@@ -5,8 +5,9 @@ import (
 	"log"
 	"net"
 	"os"
-	"signal"
+	"os/signal"
 
+	"go.mongodb.org/mongo-driver/mongo"
 	"github.com/Yepez1997/goProjects/src/gRPC/blog/blogpb"
 	"google.golang.org/grpc"
 )
@@ -16,9 +17,20 @@ type server struct{}
 
 func main() {
 
-	fmt.Print("Blog Server Started ...")
+	// get the  go code we get the file name and line number - if crashes
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+
+
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(
+	"mongodb+srv://<username>:<password>@<cluster-address>/test?w=majority"
+	))
+	if err != nil { log.Fatal(err) }
+
+	fmt.Print("Blog Server Started ...\n")
 	// create connection; and port binding
-	listener, err := net.Listen("tcp", "0.0.0.0:50051")
+	listener, err := net.Listen("tcp", "0.0.0.0:50052")
 	if err != nil {
 		log.Fatalf("Failed to listen: %v", err)
 	}
@@ -45,15 +57,14 @@ func main() {
 		if err := s.Serve(listener); err != nil {
 			log.Fatalf("Failed to serve: %v", err)
 		}
-	}
+	}()
 
-	// interrupt on signal; wait for control c to exit 
+	// interrupt on signal; wait for control c to exit
 	ch := make(chan os.Signal, 1)
 	signal.Notify(ch, os.Interrupt)
 
-
-	// block until the signal is received 
-	<-ch 
+	// block until the signal is received
+	<-ch
 	fmt.Println("Stopping the server")
 	s.Stop()
 	fmt.Println("Stopping the listener")
