8d741351acc5ccb059e5729ce13cafd01f0ff7d6
Aureliano Yepez <ayepez2020@berkeley.edu>
2019-09-12 22:32:01 -0700

    more complex channel

--- src/concurrency/channel/main.go
+++ src/concurrency/channel/main.go
@@ -3,13 +3,17 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"time"
 )
 
+// CalculateValue - send random value to the channel
 func CalculateValue(values chan int) {
 	value := rand.Intn(10)
 	fmt.Println("Calculated Random Value: {}", value)
 	// send to values
+	time.Sleep(1000 * time.Millisecond)
 	values <- value
+	fmt.Println("Only executes after another goroutine performs a receive on the channel")
 }
 
 // channels in go
@@ -17,9 +21,13 @@ func CalculateValue(values chan int) {
 func main() {
 	fmt.Println("Go Channel")
 	values := make(chan int)
+	// good practice
 	defer close(values)
+	go CalculateValue(values)
 	go CalculateValue(values)
 	// receive from values
 	value := <-values
+	// act of sending and receiving from a channel are blocking
+	// ie this program will not finish unless a value is recieved
 	fmt.Println(value)
 }
