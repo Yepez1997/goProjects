045578c2920f3b70dc637b753fb51ff5c056de4d
Aureliano Yepez <ayepez2020@berkeley.edu>
2019-09-14 13:32:39 -0700

    more complex go routines

--- src/concurrency/random/main.go
+++ src/concurrency/random/main.go
@@ -13,12 +13,15 @@ var total = 0
 var mutex = sync.Mutex{}
 
 func randomInt(i int, wg *sync.WaitGroup) {
+	// place a lock in the critical section
 	mutex.Lock()
 	fmt.Println(i)
 	total += i
 	fmt.Printf("Current total after summing %d: %d\n", i, total)
 	time.Sleep(1 * time.Second)
+	// unlock the critical section
 	mutex.Unlock()
+	// go routines finished so decrement by calling done
 	wg.Done()
 }
 
@@ -26,11 +29,28 @@ func main() {
 	var wg sync.WaitGroup // adding a wait group so that all go routines finish
 
 	numberGoRoutines := 0
-	for i := 0; i < 10; i++ {
-		wg.Add(1)
-		numberGoRoutines++
-		go randomInt(i, &wg)
-	}
+	wg.Add(1)
+	go func() {
+		for i := 0; i < 10; i++ {
+			// add to the wait group to keep track ofthe number
+			wg.Add(1)
+			numberGoRoutines++
+			go randomInt(i, &wg)
+		}
+		//time.Sleep(10 * time.Second)
+		wg.Done()
+	}()
+
+	wg.Add(1)
+	go func() {
+		for i := 0; i < 10; i++ {
+			wg.Add(1)
+			numberGoRoutines++
+			go randomInt(i, &wg)
+		}
+		wg.Done()
+	}()
+
 	wg.Wait()
 	fmt.Printf("Done with all %d go routines\n", numberGoRoutines)
 	fmt.Printf("Sum of all go routines -> %d\n", total)
