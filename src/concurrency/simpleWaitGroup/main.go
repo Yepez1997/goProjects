1cb1effdb4ee8307c9cc614374c068993732c581
Aureliano Yepez <yepez4845@gmail.com>
2019-11-26 22:39:21 -0800

    minor changes to concurreny

--- src/concurrency/simpleWaitGroup/main.go
+++ src/concurrency/simpleWaitGroup/main.go
@@ -7,7 +7,19 @@ import (
 
 func myFunc(waitgroup *sync.WaitGroup) {
 	fmt.Println("Inside my go routine")
-	// indicate the go routine has finished
+	waitgroup.Done()
+}
+
+func myFunc2(waitgroup *sync.WaitGroup) {
+	fmt.Println("Inside second go routine")
+	waitgroup.Done()
+}
+
+func forFunc(waitgroup *sync.WaitGroup, n int) {
+	fmt.Print("Inside for .... \n")
+	for i := 0; i < n; i++ {
+
+	}
 	waitgroup.Done()
 }
 
@@ -16,8 +28,17 @@ func main() {
 	fmt.Println("Hello World")
 	var waitgroup sync.WaitGroup
 	// call .add before attemtping to execute go routine
-	waitgroup.Add(1)
+	waitgroup.Add(2)
+	go myFunc2(&waitgroup)
 	go myFunc(&waitgroup)
+	n := 5
+
+	for i := 0; i < n; i++ {
+		waitgroup.Add(1)
+		// these happpen concurrently
+		go forFunc(&waitgroup, n)
+	}
+
 	// do not go futher until the go routine has finished executing
 	waitgroup.Wait()
 	// once finished print this
