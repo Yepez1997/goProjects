39071a60b2508d5a7440988e6fbd2e4ca2af8058
Aureliano Yepez <yepez4845@gmail.com>
2020-02-15 19:35:19 -0800

    properly cleaning go routines

---
+++ src/concurrency/concurrencyPatterns/badConfinement/main.go
@@ -0,0 +1,27 @@
+// not bad confinement but rather a data structure that is not concurrent safe
+// concurrent code that uti is usualy simple than lexixally confined variables
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"sync"
+)
+
+func main() {
+	printData := func(wg *sync.WaitGroup, data []byte) {
+		defer wg.Done()
+		var buff bytes.Buffer
+		for _, b := range data {
+			fmt.Fprintf(&buff, "%c", b)
+		}
+		fmt.Println(buff.String())
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+	data := []byte("golang")
+	go printData(&wg, data[:3])
+	go printData(&wg, data[3:])
+	wg.Wait()
+}
